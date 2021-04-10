b5c13a14dda9a09a12d413b23ea9736a51e46b3e
Phoenyn <65796056+Phoenyn@users.noreply.github.com>
2021-04-10 17:03:58 -0300

    Placar de Grilhos
    Feito um loop que reorganiza a posição dos grilhos com seus respectivos nomes baseado na quantidade de pulos que deram do com menos pulos para o com mais pulos. Depois o resultado é exbido.

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		
 		int minGrilos = 2;
 		int maxGrilos = 5;
+
 		
 		// Pega um número aleatório entre minGrilos e maxGrilos para determinar a quantidade de participantes
 		Random rand = new Random();
@@ -22,6 +23,42 @@ public class Main {
 			String nomeGrilo = "Grilo_0" + (i + 1);
 			threads[i] = new ThreadProcessor(nomeGrilo, maxDistancia);
 			threads[i].start();
+		}
+		
+		for (int i = 0; i < threads.length; i++) 
+		{
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		for(int i = 0 ; i < threads.length; i++)
+	    {
+	        for(int j = i+1 ; j < threads.length; j++)
+	        {
+	            if(threads[i].qntPulos > threads[j].qntPulos)
+	            {
+	                int temp = threads[i].qntPulos;
+	                threads[i].qntPulos = threads[j].qntPulos;
+	                threads[j].qntPulos = temp;
+	                String tempNome = threads[i].nome;
+	                threads[i].nome = threads[j].nome;
+	                threads[j].nome = tempNome;
+	            }
+	        }
+	    }
+		
+		System.out.println("");
+		System.out.println("Placar:");
+		System.out.println("");
+		
+		for (int i = 0; i < threads.length; i++) 
+		{
+			int placar = i +1;
+			System.out.println("O " + threads[i].nome + " ficou em " + placar + "º lugar com " + threads[i].qntPulos + " pulos");
 		}
 	}
 
