0057e78225446072c2fd6c8e44a1884318fae108
Juliana Potengy <julianalpotengy@gmail.com>
2021-04-17 15:53:30 -0300

    Mudança do conteúdo do Main para CorridaDeGrilo
    Essa mudança foi realizada para ser possível passar as informações para as threads.
    O antigo placar está comentado para possível utilização futura.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,60 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		int minGrilos = 2;
-		int maxGrilos = 5;
-
-		
-		// Pega um número aleatório entre minGrilos e maxGrilos para determinar a quantidade de participantes
-		Random rand = new Random();
-		int nGrilos = rand.nextInt((maxGrilos - minGrilos) + 1) + minGrilos;
-		
-		int maxDistancia = 100;
-		
-		// Cria as threads
-		ThreadProcessor threads[] = new ThreadProcessor[nGrilos];
-		
-		for(int i = 0; i < nGrilos; i++) {
-			String nomeGrilo = "Grilo_0" + (i + 1);
-			threads[i] = new ThreadProcessor(nomeGrilo, maxDistancia);
-			threads[i].start();
-		}
-		
-		for (int i = 0; i < threads.length; i++) 
-		{
-			try {
-				threads[i].join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		for(int i = 0 ; i < threads.length; i++)
-	    {
-	        for(int j = i+1 ; j < threads.length; j++)
-	        {
-	            if(threads[i].qntPulos > threads[j].qntPulos)
-	            {
-	                int temp = threads[i].qntPulos;
-	                threads[i].qntPulos = threads[j].qntPulos;
-	                threads[j].qntPulos = temp;
-	                String tempNome = threads[i].nome;
-	                threads[i].nome = threads[j].nome;
-	                threads[j].nome = tempNome;
-	            }
-	        }
-	    }
-		
-		System.out.println("");
-		System.out.println("Placar:");
-		System.out.println("");
-		
-		for (int i = 0; i < threads.length; i++) 
-		{
-			int placar = i +1;
-			System.out.println("O " + threads[i].nome + " ficou em " + placar + "º lugar com " + threads[i].qntPulos + " pulos");
-		}
+		CorridaDeGrilo corrida = new CorridaDeGrilo();
 	}
 
 }
--- src/main/ThreadProcessor.java
+++ src/main/ThreadProcessor.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 public class ThreadProcessor extends Thread {
 
+	CorridaDeGrilo corrida;
+	
 	String nome;
 	int distanciaAtual;
 	int maxDistancia;
@@ -12,11 +14,12 @@ public class ThreadProcessor extends Thread {
 	int minDistPulo = 5;
 	int maxDistPulo = 20;
 	
-	public ThreadProcessor(String n, int md) {
+	public ThreadProcessor(String n, int md, CorridaDeGrilo c) {
 		nome = n;
 		distanciaAtual = 0;
 		maxDistancia = md;
 		qntPulos = 0;
+		corrida = c;
 	}
 	
 	@Override
